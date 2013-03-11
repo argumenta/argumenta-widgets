f900c09c29563f56537b503676d55ff3362de1bb
Tyler Florez <qualiabyte@gmail.com>
2013-03-10 23:25:33 -0700

    cleanup Sidebar

--- assets/js/argumenta/widgets/Sidebar.js
+++ assets/js/argumenta/widgets/Sidebar.js
@@ -10,6 +10,7 @@ function( $, Base, Template ) {
 
         moduleID: 'Sidebar',
         template: Template,
+
         defaults: {
             "max-width": "50%",
             "rightBound": 5,
@@ -19,57 +20,62 @@ function( $, Base, Template ) {
 
         init: function( options, originalElem ) {
             var self = this;
-
-            // Save the original element and refresh
             self.originalElement = originalElem;
             self._refresh();
         },
 
         prototype: {
 
-            // Override Base._bindUI
+            // Binds UI behavior after refresh.
             _bindUI: function() {
                 var self = this;
 
-                // Wait until DOM insertion for parent-dependent init
+                // Wait until DOM insertion for parent-dependent init.
                 setTimeout( function() {
                     var parent = self.element.parent();
+
                     if ( parent ) {
-                        // Make the parent our context for absolute positioning
+                        // Make the parent our context for absolute positioning.
                         self.element.parent().css( { "position": "relative" } );
 
-                        // Initialize our size and state
+                        // Initialize our size and state.
                         self.updateSize();
                         self.update();
-                        //console.log( 'Sidebar parent: ' + parent );
+
+                        if (Sidebar.DEBUG) {
+                            console.log('Sidebar parent: ' + parent);
+                        }
                     }
                     else {
                         setTimeout( arguments.callee, 100 );
-                        //console.log( 'Sidebar waiting for insertion...' );
+                        if (Sidebar.DEBUG) {
+                            console.log('Sidebar waiting for insertion...');
+                        }
                     }
                 }, 100 );
 
-                // Nest the contents of the originally activated element
+                // Nest the contents of the originally activated element.
                 if ( self.originalElement ) {
-                    self.element.find('.sidebar-contents')
-                                .empty()
-                                .append( self.originalElement.children() );
+                    self.element
+                      .find('.sidebar-contents')
+                      .empty()
+                      .append( self.originalElement.children() );
                 }
 
-                // Update sidebar size on window resize
+                // Update sidebar size on window resize.
                 $( window ).resize( function() {
-                    // Resize & clear assumption about our relative position
                     self.lastPosition = null;
                     self.updateSize();
                     self.update();
                 } );
 
-                // Update sidebar on scroll
+                // Update sidebar on scroll.
                 $( window ).scroll( function () {
                     self.update();
                 } );
             },
 
+            // Updates size of sidebar.
             updateSize: function() {
                 var self = this;
                 var win = $( window );
@@ -77,33 +83,34 @@ function( $, Base, Template ) {
                 var parent = self.element.parent();
                 var prev = self.element.prev();
 
-                // Record the parent and previous size on last update
+                // Record the parent and previous size on last update.
                 self.lastParentHeight = parent.height();
                 self.lastParentWidth = parent.innerWidth();
                 self.lastPrevWidth = prev.outerWidth();
 
-                // Calculate our new width
+                // Calculate our new width.
                 self.width = parent.innerWidth() - prev.outerWidth() - self.options.rightBound;
 
-                // Update widget size
+                // Update widget size.
                 self.element.css( {
                     "max-height": winHeight,
                     "max-width": self.options["max-width"],
                     "width": self.width
                 } );
 
-                // Update parent size
+                // Update parent size.
                 self.element.parent().css( {
                     "min-height": winHeight
                 } );
 
-                // Leave space for content scrollbar on the right
+                // Leave space for content scrollbar on the right.
                 self.element.children('.sidebar-container').css( {
                     "float": "left",
                     "width": self.element.width() - self.options.scrollbarWidth + "px"
                 } );
             },
 
+            // Updates sidebar state.
             update: function() {
                 var self = this;
                 var parent = self.element.parent();
@@ -114,7 +121,7 @@ function( $, Base, Template ) {
                      self.lastParentWidth !== parent.innerWidth() ||
                      self.lastPrevWidth !== prev.outerWidth()
                    ) {
-                    // Clear assumption about position & update size
+                    // Clear assumption about position & update size.
                     self.lastPosition = null;
                     self.updateSize();
                 }
@@ -127,10 +134,9 @@ function( $, Base, Template ) {
                 self.bottom = self.winTop + self.options.topBound + self.element.height();
                 self.leftBound = prev.offset().left + prev.outerWidth();
 
-                // Floating sidebar
-                // Follows the window view, within the bounds of its parent.
-                // Anchors to the parent boundaries at the top & bottom.
-                // Uses "position: fixed" for smooth scrolling in the middle case.
+                // Floating sidebar follows the window view, within the parent.
+                // It anchors to the parent boundaries at top and bottom.
+                // Position is fixed for smooth scrolling in the middle case.
                 // Manipulates DOM only as boundaries reached, for speed & smoothness.
                 if ( self.winTop + self.options.topBound < self.parentTop ) {
                     if ( self.lastPosition !== "top" ) {
@@ -142,7 +148,9 @@ function( $, Base, Template ) {
                             "width": self.width + "px"
                         } );
                         self.lastPosition = "top";
-                        //console.log("Sidebar in top position");
+                        if (Sidebar.DEBUG) {
+                            console.log("Sidebar in top position.");
+                        }
                     }
                 }
                 else if ( self.bottom > self.parentBottom && self.lastPosition !== "top" ||
@@ -156,7 +164,9 @@ function( $, Base, Template ) {
                             "width": self.width + "px"
                         } );
                         self.lastPosition = "bottom";
-                        //console.log("Sidebar in bottom position", self);
+                        if (Sidebar.DEBUG) {
+                            console.log("Sidebar in bottom position.", self);
+                        }
                     }
                 }
                 else {
@@ -169,13 +179,16 @@ function( $, Base, Template ) {
                             "width": self.width + "px"
                         } );
                         self.lastPosition = "middle";
-                        //console.log("Sidebar in middle position");
+                        if (Sidebar.DEBUG) {
+                            console.log("Sidebar in middle position.");
+                        }
                     }
                 }
             }
         },
 
         "static": {
+            DEBUG: false
         }
     } );
 
