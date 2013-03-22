7101e1ed70c6efd09dcacf2c2e9ecc81b9711b0e
Tyler Florez <qualiabyte@gmail.com>
2013-03-21 18:37:25 -0700

    Add tests for Argument properties.

--- test/fixtures/index.js
+++ test/fixtures/index.js
@@ -34,5 +34,18 @@ define([
         return new Argument(data);
     };
 
+    Fixtures.validCommitData = function() { return {
+        object_type: "commit",
+        sha1: "57f61273647ef3bf4152ada12ea8b68f9cea2f36",
+        target_type: 'argument',
+        target_sha1: '39cb3925a38f954cf4ca12985f5f948177f6da5e',
+        committer: 'tester',
+        commit_date: '1970-01-01T00:00:00Z',
+        parent_sha1s: [
+            '0123456789abcdef000000000000000000000000',
+            '1a1a1a1a1a1a1a1a000000000000000000000000'
+        ]
+    }; };
+
     return Fixtures;
 });
--- test/unit/ArgumentTest.js
+++ test/unit/ArgumentTest.js
@@ -21,11 +21,61 @@ function(chai, fixtures, Argument, Base) {
         });
 
         describe('new Argument( options, element )', function() {
+
             it('should return a new Argument widget', function() {
                 var argData = fixtures.validArgumentData();
                 var argument = new Argument(argData);
                 assert.instanceOf(argument, Argument);
                 assert.instanceOf(argument, Base);
+            });
+
+            it('should display the argument title', function() {
+                var data = fixtures.validArgumentData();
+                var argument = new Argument(data);
+                var html = argument.element.html();
+                assert.include(
+                    html, data.title,
+                    'Check title.'
+                );
+            });
+
+            it('should display the name of the committer', function() {
+                var data = fixtures.validArgumentData();
+                data.commit = fixtures.validCommitData();
+                var argument = new Argument(data);
+                var link = argument.element.find('.committer a').first();
+                var html = link.html();
+                assert.include(
+                    html, data.commit.committer,
+                    'Check committer.'
+                );
+            });
+
+            it('should display the commit date', function() {
+                var data = fixtures.validArgumentData();
+                data.commit = fixtures.validCommitData();
+                var argument = new Argument(data);
+                var span = argument.element.find('.commit-date').first();
+                var html = span.html();
+                assert.include(
+                    html, data.commit.commit_date,
+                    'Check commit date.'
+                );
+            });
+
+            it('should display a link to argument by SHA-1', function() {
+                var data = fixtures.validArgumentData();
+                var argument = new Argument(data);
+                var link = argument.element.find('.argument-sha1 a').first();
+                var html = link.html();
+                assert.include(
+                    link.prop('href'), '/arguments/' + data.sha1,
+                    'Check SHA-1 link.'
+                );
+                assert.include(
+                    html, data.sha1.substr(0, 20),
+                    'Check SHA-1.'
+                );
             });
         });
 
