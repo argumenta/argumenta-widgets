743bc142aa4c8cd11f16ebda2a1b153114af4b92
Tyler Florez <qualiabyte@gmail.com>
2013-03-28 00:27:31 -0700

    Add tests for proposition display.

--- test/unit/PropositionTest.js
+++ test/unit/PropositionTest.js
@@ -35,6 +35,44 @@ function(chai, undefined, fixtures, Proposition, Base) {
                     'Proposition widgets are instances of Proposition.'
                 );
             });
+
+            it('should display the proposition index', function() {
+                var data = fixtures.validPropositionData();
+                var proposition = new Proposition(data);
+                var $span = proposition.element.find('span.index');
+                var html = $span.html();
+                assert.include(
+                    html, 'P.',
+                    'Check index.'
+                );
+            });
+
+            it('should display the proposition text', function() {
+                var data = fixtures.validPropositionData();
+                var proposition = new Proposition(data);
+                var $span = proposition.element.find('span.text');
+                var html = $span.html();
+                assert.include(
+                    html, data.text,
+                    'Check text.'
+                );
+            });
+
+            it('should display the proposition SHA-1', function() {
+                var data = fixtures.validPropositionData();
+                var proposition = new Proposition(data);
+                var $link = proposition.element.find('span.sha1 a');
+                var href = $link.prop('href');
+                var sha1 = $link.html();
+                assert.include(
+                    href, '/propositions/' + data.sha1,
+                    'Check object link.'
+                );
+                assert.include(
+                    sha1, data.sha1.substr(0, 20),
+                    'Check SHA-1.'
+                );
+            });
         });
     });
 });
