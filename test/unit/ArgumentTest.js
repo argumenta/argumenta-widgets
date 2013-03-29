31dab8a6c93840b0acaddf418dcbcc107500c7ac
Tyler Florez <qualiabyte@gmail.com>
2013-03-29 02:53:51 -0700

    Organize tests for argument methods.

--- test/unit/ArgumentTest.js
+++ test/unit/ArgumentTest.js
@@ -134,6 +134,60 @@ function(chai, undefined, fixtures, Argument, Proposition, Base) {
             });
         });
 
+        describe('getType()', function() {
+            it('should return the object type', function() {
+                var argument = fixtures.validArgument();
+                assert.equal(argument.getType(), 'argument');
+            });
+        });
+
+        describe('getSha1()', function() {
+            it('should return the object sha1', function() {
+                var argument = fixtures.validArgument();
+                assert.equal(
+                    argument.getSha1(),
+                    '7077e1ce31bc8e9d2a88479aa2d159f2f9de4856',
+                    'Check SHA1.'
+                );
+            });
+        });
+
+        describe('getPropositionWidgets()', function() {
+            it('should return a widget for each proposition', function() {
+                var data = fixtures.validArgumentData();
+                var argument = new Argument(data);
+                var props = argument.getPropositionWidgets();
+                assert.lengthOf(
+                    props, data.propositions.length,
+                    'Returns correct number of objects.'
+                );
+                for (var i = 0; i < props.length; i++) {
+                    var p = props[i];
+                    assert.instanceOf(
+                        p, Proposition,
+                        'Each object returned is a proposition widget.'
+                    );
+                }
+            });
+        });
+
+        describe('setPropositions( props )', function() {
+            it('should load the given propositions data', function() {
+                var data = fixtures.validArgumentData();
+                var argument = new Argument(data);
+                argument.setPropositions([]);
+                assert.deepEqual(
+                    argument.propositions, [],
+                    'Clear propositions.'
+                );
+                argument.setPropositions(data.propositions);
+                assert.deepEqual(
+                    argument.propositions, data.propositions,
+                    'Set propositions.'
+                );
+            });
+        });
+
         describe('Argument propositions', function() {
 
             it('should show propositions when option is set', function() {
@@ -226,60 +280,6 @@ function(chai, undefined, fixtures, Argument, Proposition, Base) {
                     'Widget remains in DOM.'
                 );
                 done();
-            });
-        });
-
-        describe('getType()', function() {
-            it('should return the object type', function() {
-                var argument = fixtures.validArgument();
-                assert.equal(argument.getType(), 'argument');
-            });
-        });
-
-        describe('getSha1()', function() {
-            it('should return the object sha1', function() {
-                var argument = fixtures.validArgument();
-                assert.equal(
-                    argument.getSha1(),
-                    '7077e1ce31bc8e9d2a88479aa2d159f2f9de4856',
-                    'Check SHA1.'
-                );
-            });
-        });
-
-        describe('setPropositions( props )', function() {
-            it('should load the given propositions data', function() {
-                var data = fixtures.validArgumentData();
-                var argument = new Argument(data);
-                argument.setPropositions([]);
-                assert.deepEqual(
-                    argument.propositions, [],
-                    'Clear propositions.'
-                );
-                argument.setPropositions(data.propositions);
-                assert.deepEqual(
-                    argument.propositions, data.propositions,
-                    'Set propositions.'
-                );
-            });
-        });
-
-        describe('getPropositionWidgets()', function() {
-            it('should return a widget for each proposition', function() {
-                var data = fixtures.validArgumentData();
-                var argument = new Argument(data);
-                var props = argument.getPropositionWidgets();
-                assert.lengthOf(
-                    props, data.propositions.length,
-                    'Returns correct number of objects.'
-                );
-                for (var i = 0; i < props.length; i++) {
-                    var p = props[i];
-                    assert.instanceOf(
-                        p, Proposition,
-                        'Each object returned is a proposition widget.'
-                    );
-                }
             });
         });
     });
