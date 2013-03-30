46acb005f4ac006a01e1116025e42819cbc3a586
Tyler Florez <qualiabyte@gmail.com>
2013-03-30 06:34:14 -0700

    Add tests for AddTag accessors.

--- test/unit/AddTagTest.js
+++ test/unit/AddTagTest.js
@@ -43,5 +43,65 @@ function(chai, undefined, fixtures, AddTag, Base) {
                 );
             });
         });
+
+        describe('getTagType()', function() {
+            it('should return the tag type', function() {
+                var data = fixtures.validAddTagData();
+                var addTag = new AddTag(data);
+                assert.equal(
+                    addTag.getTagType(),
+                    data.tag_type,
+                    'Check tag type.'
+                );
+            });
+        });
+
+        describe('getTargetType()', function() {
+            it('should return the target type', function() {
+                var data = fixtures.validAddTagData();
+                var addTag = new AddTag(data);
+                assert.equal(
+                    addTag.getTargetType(),
+                    data.target_type,
+                    'Check target type.'
+                );
+            });
+        });
+
+        describe('getTargetSha1()', function() {
+            it('should return the target SHA-1', function() {
+                var data = fixtures.validAddTagData();
+                var addTag = new AddTag(data);
+                assert.equal(
+                    addTag.getTargetSha1(),
+                    data.target_sha1,
+                    'Check target SHA-1.'
+                );
+            });
+        });
+
+        describe('getSourceType()', function() {
+            it('should return the source type', function() {
+                var data = fixtures.validAddTagData();
+                var addTag = new AddTag(data);
+                assert.equal(
+                    addTag.getSourceType(),
+                    data.source_type,
+                    'Check source type.'
+                );
+            });
+        });
+
+        describe('getSourceSha1()', function() {
+            it('should return the source SHA-1', function() {
+                var data = fixtures.validAddTagData();
+                var addTag = new AddTag(data);
+                assert.equal(
+                    addTag.getSourceSha1(),
+                    data.source_sha1,
+                    'Check source SHA-1.'
+                );
+            });
+        });
     });
 });
