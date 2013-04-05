ae7a5f140178d7d7ff81781e8ef82a52a2159869
Tyler Florez <qualiabyte@gmail.com>
2013-04-04 20:58:03 -0700

    Update build configuration.

--- assets/js/app.build.js
+++ assets/js/app.build.js
@@ -8,8 +8,22 @@
         }
     ],
     paths: {
-        'jquery'        : "jquery-1.7.1.min",
-        'jquery-ui'     : "jquery-ui-1.8.16.custom.min",
-        'mustache'      : "require-mustache"
+        'jquery'                : "jquery-1.7.1.min",
+        'jquery-ui'             : "jquery-ui-1.8.16.custom.min",
+        'jquery-temporaryClass' : "jquery.temporaryClass",
+        'mustache'              : "require-mustache"
+    },
+    shim: {
+        'jquery' : {
+            exports: "jQuery",
+        },
+        'jquery-ui' : {
+            exports: "jQuery.ui",
+            deps: ["jquery"],
+        },
+        'jquery-temporaryClass' : {
+            exports: "jQuery.fn.temporaryClass",
+            deps: ["jquery"]
+        }
     }
 })
